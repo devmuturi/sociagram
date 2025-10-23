19233cdf27aacbe8cdb82823ae06818d7c3e5a8b
devmuturi <muturinjonge09@gmail.com>
2025-10-23 21:45:16 +0300

    feat:adding user crud api endpoints

--- app/controllers/api/users/users_controller.rb
+++ app/controllers/api/users/users_controller.rb
@@ -1,2 +1,39 @@
 class Api::Users::UsersController < ApplicationController
+  before_action :set_user, only: [ :show, :update, :destroy ]
+
+  # GET /api/users
+  def index
+    @users = User.all
+    render json: @users
+  end
+
+  # GET /api/users/:id
+  def show
+    render json: @user
+  end
+
+  # PUT /api/users/:id
+  def update
+    if @user.update(user_params)
+      render json: @user
+    else
+      render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /api/users/:id
+  def destroy
+    @user.destroy
+    head :no_content
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,12 @@ Rails.application.routes.draw do
     sessions: "users/sessions",
     registrations: "users/registrations"
   }
+
+  namespace :api do
+    namespace :users do
+      resources :users, only: [ :show, :update, :destroy ]
+    end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
