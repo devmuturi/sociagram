550c1da4e56cf728cb4dac498ddd97231b916a85
devmuturi <muturinjonge09@gmail.com>
2025-10-23 18:24:26 +0300

    Setup devise and rspec

--- .history/config/routes_20251023175735.rb
+++ .history/config/routes_20251023175735.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
-  namespace :api do
-    devise_for :users, path: "users", controllers: {
-      sessions: "api/users/sessions",
-      registrations: "api/users/registrations"
-    }
-  end
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
