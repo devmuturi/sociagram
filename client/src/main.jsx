837029dbd73a1e369820a622afb5dd27732746ca
devmuturi <muturinjonge09@gmail.com>
2025-10-24 21:02:40 +0300

    Add material ui

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,37 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { ThemeProvider, createTheme } from '@mui/material/styles'
+import { pink } from "@mui/material/colors"
 import './index.css'
 import App from './App.jsx'
 
+const theme = createTheme({
+  typography: { 
+    useNextVariants: true, 
+  }, 
+  palette: { 
+    primary: { 
+    light: '#5c67a3', 
+    main: '#3f4771', 
+    dark: '#2e355b', 
+    contrastText: '#fff', 
+  }, 
+  secondary: { 
+    light: '#ff79b0', 
+    main: '#ff4081', 
+    dark: '#c60055', 
+    contrastText: '#000', 
+  }, 
+    openTitle: '#3f4771', 
+    protectedTitle: pink['400'], 
+    type: 'light' 
+  } 
+})
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </StrictMode>,
 )
