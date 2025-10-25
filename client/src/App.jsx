3f9bfdef167a54925f8be75ebc815819bee44afd
devmuturi <muturinjonge09@gmail.com>
2025-10-25 14:49:33 +0300

    feat: Architecture the frontend file structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,9 @@
-import './App.css'
-import Button from './components/Button'
+import { Home } from "./pages/Home"
 
 function App() {
   return (
     <>
-      <Button/>
+      <Home/>
     </>
   )
 }
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,9 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { ThemeProvider, createTheme } from '@mui/material/styles'
+import { RouterProvider } from "react-router"
 import { pink } from "@mui/material/colors"
 import './index.css'
-import App from './App.jsx'
+import { router } from './routes/MainRouter'
 
 const theme = createTheme({
   typography: { 
@@ -31,7 +32,7 @@ const theme = createTheme({
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ThemeProvider theme={theme}>
-      <App />
+      <RouterProvider router={router}/>
     </ThemeProvider>
   </StrictMode>,
 )
