f26cab955cf65a530361fb07be4f13d027025f33
devmuturi <muturinjonge09@gmail.com>
2025-10-27 22:58:26 +0300

    Model the api

--- client/src/apis/authApi.js
+++
@@ -1,9 +0,0 @@
-import api from "./axios";
-
-const authApi = {
-    login: (data) => api.post("/login", data),
-    signup: (data) => api.post("/signup", data),
-    logout: () => api.delete("/logout") 
-}
-
-export default authApi;
--- client/src/context/AuthContext.jsx
+++
@@ -1,7 +0,0 @@
-// src/context/AuthContext.jsx
-import { createContext, useContext } from "react";
-
-export const AuthContext = createContext(null);
-
-// Custom hook
-export const useAuth = () => useContext(AuthContext);
