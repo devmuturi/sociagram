d7602dae41462a6da7b963703dc6fa6313923a40
devmuturi <muturinjonge09@gmail.com>
2025-10-27 23:37:22 +0300

    feat: post model for posts

---
+++ app/controllers/api/v1/posts_controller.rb
@@ -0,0 +1,50 @@
+class Api::V1::PostsController < ApplicationController
+  before_action :set_post, only: [ :show, :update, :destroy ]
+
+  # GET /api/v1/posts
+  def index
+    @posts = Post.all
+    render json: @posts
+  end
+
+  # GET /api/v1/post/:id
+  def show
+    render json: @post
+  end
+
+  # POST /api/v1/posts
+  def create
+    @post = current_user.posts.build(post_params)
+
+    if @post.save
+      render json: @post, status: :created
+    else
+      render json: @post.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH /api/v1/posts/1
+  def update
+    if @post.update(post_params)
+      render json: @post
+    else
+      render json: @post.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /api/v1/posts/1
+  def destroy
+    @post.destroy
+    head :no_content
+  end
+
+  private
+
+  def set_post
+    @post = current_user.posts.find[params[:id]]
+  end
+
+  def post_params
+    params.expect(post: [ :content ])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,10 @@ Rails.application.routes.draw do
   }
 
   namespace :api do
-    resources :users, only: [ :index, :show, :update, :destroy ]
+    namespace :v1 do
+      resources :users, only: [ :index, :show, :update, :destroy ]
+      resources :posts
+    end
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
